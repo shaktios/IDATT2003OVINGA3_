79d1622d3dd3ad7d98d5749d94611b3030091d5d
Shakti Om Sharma <shaktiforgithub@gmail.com>
2025-02-17 00:37:29 +0100

    Startet Maven-prosjekt på nytt

---
+++ textcommands/src/main/java/no/ntnu/idatt2003/App.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatt2003;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
